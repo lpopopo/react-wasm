417c65a5f12a9d59dbfeabe189c57fba6bdb08c8
lpopopo <2403264743@qq.com>
2022-10-11 09:49:46 +0800

    fix:app

--- src/App.js
+++ src/App.js
@@ -1,49 +1,32 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
+function App() {
 
-class App extends React.PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      nativeModule: null,
-    };
-
-    this.showGreet = this.showGreet.bind(this);
+  const handleClick = () => {
+    import("add").then(({ greet }) => {
+      greet()
+    })
   }
 
-  componentDidMount() {
-    import("add").then(native => {
-      this.setState({
-        nativeModule: native,
-      });
-    });
-  }
-
-  showGreet() {
-    const {
-      nativeModule,
-    } = this.state;
-
-    if (!nativeModule) {
-      alert("Please try after some time...");
-    } else {
-      nativeModule.greet("Human");
-    }
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          WebAssembly with Rust and React (Using create-react-app)
-          <button onClick={this.showGreet}>
-            Show greet from Rust
-          </button>
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <button onClick={() => handleClick()}>show greet from react</button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
